bc455fff610e5be74f90d690cddbb45192348713
RequintelRoseAnn <roseannrequintel7@gmail.com>
2022-05-25 13:12:20 +0800

    Song1 Added

---
+++ song1.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <link href="css/styles.css" rel="stylesheet" />
+        <style>
+        body {
+                 background-color: darkslateblue;
+            }
+        </style>
+
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-2">
+                <a class="navbar-brand" href="#page-top">WebLyrics</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                       <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song_1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song_2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song_3</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song_4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song_5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Roar</h1>
+                <p class="lead">Song by: Katty Perry</p>
+                
+            </div>
+        
+
+        </header>
+
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-7 text-center text-white">
+                      
+                    
+                        <?php
+                        //lyrics
+                        echo "<h2 class='text-white'>'LYRICS'</h2>";  
+                        echo "<p class='lead'></p>";
+                        echo "<br/>";
+
+                        echo "<p class='small'>[Verse 1]</p>";
+                        echo "<p>[G]I used to bite my tongue and hold my breath </p>";
+                        echo "<p> Scared to rock the [Am]boat and make a mess </p>";
+                        echo "<p> [Em]So I sat quietly, agree [C]politely </p>";
+                        echo "<p>  [G]I guess that I forgot I had a choice </p>";
+                        echo "<p> I let you push me [Am]past the breaking point </p>";
+                        echo "<p>[Em]I stood for nothing, so I fell for [C]everything </p>";
+                        echo "<p>  You [G]held me down but I got up (hey!)</p>";
+                        echo "<p> Already brushing [Am]off the dust </p>";
+                        echo "<p> You [Em]hear my voice, you hear that sound </p>"; 
+                        echo "<p> Like thunder gonna’ [C]shake the crowd </p>";
+                        echo "<p>  You [G]held me down but I got up (hey!) </p>";
+                        echo "<p>  Get ready cos’ I’ve [Am]had enough </p>";
+                        echo "<p> I [Em]see it all, I see it now </p>";
+                       echo "<br/>";
+
+                        echo "<p class='small'>[Chorus ]</p>";
+                        echo "<p>  I got the [C]eye of the [G]ti--ger, the fighter, dancing through the [Am]fi—re </p>";
+                        echo "<p> Cos’ I [Em]am a champion, and you’re gonna’ hear [C]me [G]roar  </p>";
+                        echo "<p> Louder, louder than a [Am]lion, cos’ I [Em]am a champion </p>";
+                        echo "<p> And you’re gonna’ hear [C]me [G]roar [Am](oh oh [Em]oh) You’re gonna’ hear [C]me [G]roar  </p>";
+                        echo "<br/>";
+
+                        echo "<p class='small'>[Verse 2]</p>";
+                        echo "<p> [G]Now I’m floating like a  </p>";
+                        echo "<p> butterfly, stinging like a [Am]bee, I earned my stripe </p>";
+                        echo "<p>[Em]I went from zero, to my own [C]hero  </p>";
+                        echo "<p>[G]You held me down but I got up </p>";
+                        echo "<p> You [G]held me down but I got up (hey!) </p>";
+                        echo "<p> Already brushing [Am]off the dust </p>";
+                        echo "<p> You [Em]hear my voice, you hear that sound </p>";
+                        echo "<p> Like thunder gonna’ [C]shake the crowd  </p>";
+                        echo "<p> You [G]held me down but I got up (hey!)</p>"; 
+                        echo "<p> Get ready cos’ I’ve [Am]had enough </p>";
+                        echo "<p>  I [Em]see it all, I see it now </p>";
+                        echo "<br/>";
+
+                        echo "<p class='small'>[Chorus ]</p>";
+                        echo "<p>  I got the [C]eye of the [G]ti--ger, the fighter, dancing through the [Am]fi—re </p>";
+                        echo "<p> Cos’ I [Em]am a champion, and you’re gonna’ hear [C]me [G]roar  </p>";
+                        echo "<p> Louder, louder than a [Am]lion, cos’ I [Em]am a champion </p>";
+                        echo "<p> And you’re gonna’ hear [C]me [G]roar [Am](oh oh [Em]oh) You’re gonna’ hear [C]me [G]roar  </p>";
+
+                        echo "<p>[G]Roar, [Am]roar, [Em]roar, roar, [D]roaaaar! </p>";
+                        echo "<p>  I got the [C]eye of the [G]ti--ger, the fighter, dancing through the [Am]fi—re  </p>";
+                        echo "<p> Cos’ I [Em]am a champion, and you’re gonna’ hear [C]me [G]roar  </p>";
+                        echo "<p> Louder, louder than a [Am]lion, cos’ I [Em]am a champion </p>";
+                        echo "<p> And you’re gonna’ hear [C]me [G]roar [Am](oh oh [Em]oh) You’re gonna’ hear [C]me [G]roar  </p>";
+                        echo "<br/>";
+
+
+                       
+                        ?>
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-dark">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Rose Ann Requintel</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
