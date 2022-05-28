539e738e64dbb3d5e5140486680a733bfc3912e7
RequintelRoseAnn <roseannrequintel7@gmail.com>
2022-05-28 17:46:39 +0800

    Song4 page Added

---
+++ song4.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <link href="css/styles.css" rel="stylesheet" />
+        <style>
+        body {
+                 background-color: darkblue;
+            }
+        </style>
+
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-2">
+                <a class="navbar-brand" href="#page-top">WebLyrics</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                       <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song_1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song_2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song_3</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song_4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song_5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Fireworks</h1>
+                <p class="lead">Song by: Katty Perry</p>
+                
+            </div>
+        
+
+        </header>
+
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-7 text-center text-white">
+                      
+                    
+                        <?php
+                        //lyrics 
+                        echo "<h2 class='text-white'>'LYRICS'</h2>";   
+                        echo "<p class='lead'></p>"; 
+                        echo "<br/>"; 
+ 
+                        echo "<p class='small'>[verse 1]</p>"; 
+                        echo "<p>[G] Do you ever feel [Am] like a plastic bag </p>"; 
+                        echo "<p>[Em] Drifting through the wind, [C] wanting to start again?  </p>"; 
+                        echo "<p>[G] Do you ever feel, [Am] feel so paper thin  </p>"; 
+                        echo "<p>[Em] Like a house of cards, one [C] blow from caving in?  </p>"; 
+                        echo "<p> [G] Do you ever feel [Am] already buried deep?</p>";
+                        echo "<p> [Em] Six feet under screams but [C] no one seems to hear a thing </p>";
+                        echo "<p> [G] Do you know that there's [Am] still a chance for you </p>";
+                        echo "<p> [Em] 'Cause there's a spark in [C] you? </p>";
+
+                        echo "<br/>"; 
+ 
+                        echo "<p class='small'>[Pre-Chorus]</p>"; 
+                        echo "<p> You just gotta [G] ignite the [Am] light and [Em] let it [C] shine </p>"; 
+                        echo "<p> Just [G] own the [Am] night like the [Em] 4th of [C] July </p>"; 
+                        echo "<br/>"; 
+ 
+                        echo "<p class='small'>[Chorus ]</p>"; 
+                        echo "<p> 'Cause baby, you're a [G] fire-[Am] work</p>"; 
+                        echo "<p> Come on, show 'em [Em] what you're [C] worth </p>"; 
+                        echo "<p> Make 'em go, [G] oh oh [Am] oh </p>"; 
+                        echo "<p> As you shoot across the [Em] sky-y-[C] y </p>"; 
+                        echo "<p> Baby, you're a [G] fire-[Am] work </p>"; 
+                        echo "<p> Come on, let your [Em] colors [C] burst </p>"; 
+                        echo "<p> Make 'em go, [G] oh oh [Am] oh </p>"; 
+                        echo "<p> You're gonna leave 'em all in [Em] awe, awe, [C] awe  </p>"; 
+                        echo "<br/>"; 
+ 
+                        echo "<p class='small'>[Verse 2]</p>"; 
+                        echo "<p> [G] You don't have to feel [Am] like a waste of space  </p>"; 
+                        echo "<p> [Em] You're original, [C] cannot be replaced </p>"; 
+                        echo "<p> [G] If you only knew [Am] what the future holds </p>"; 
+                        echo "<p> [Em] After a hurricane [C] comes a rainbow </p>"; 
+                        echo "<p> [G] Maybe you're reason why [Am] all the doors are closed  </p>";
+                        echo "<p> [Em] So you could open one that [C] leads you to the perfect road  </p>";
+                        echo "<p> [G] Like a lightning bolt, [Am] your heart will blow  </p>";
+                        echo "<p> [Em] And when it's time you'll [C] know </p>";
+
+                        echo "<br/>"; 
+                        echo "<p class='small'>[Pre-Chorus]</p>"; 
+                        echo "<p> You just gotta [G] ignite the [Am] light and [Em] let it [C] shine </p>"; 
+                        echo "<p> Just [G] own the [Am] night like the [Em] 4th of [C] July </p>"; 
+                        echo "<br/>"; 
+                        echo "<p class='small'>[Chorus ]</p>"; 
+                        echo "<p> 'Cause baby, you're a [G] fire-[Am] work</p>"; 
+                        echo "<p> Come on, show 'em [Em] what you're [C] worth </p>"; 
+                        echo "<p> Make 'em go, [G] oh oh [Am] oh </p>"; 
+                        echo "<p> As you shoot across the [Em] sky-y-[C] y </p>"; 
+                        echo "<p> Baby, you're a [G] fire-[Am] work </p>"; 
+                        echo "<p> Come on, let your [Em] colors [C] burst </p>"; 
+                        echo "<p> Make 'em go, [G] oh oh [Am] oh </p>"; 
+                        echo "<p> You're gonna leave 'em all in [Em] awe, awe, [C] awe  </p>"; 
+                        echo "<br/>"; 
+
+
+ 
+                        echo "<p class='small'>[bridge]</p>"; 
+                        echo "<p> [Em] Boom, boom, boom  </p>"; 
+                        echo "<p> Even brighter than the [C] moon, moon, moon</p>"; 
+                        echo "<p> It's always been inside of [G] you, you, you </p>"; 
+                        echo "<p>And now it's time to let it [D] through </p>"; 
+                        echo "<br/>"; 
+                        echo "<p class='small'>[Chorus ]</p>"; 
+                        echo "<p> 'Cause baby, you're a [G] fire-[Am] work</p>"; 
+                        echo "<p> Come on, show 'em [Em] what you're [C] worth </p>"; 
+                        echo "<p> Make 'em go, [G] oh oh [Am] oh </p>"; 
+                        echo "<p> As you shoot across the [Em] sky-y-[C] y </p>"; 
+                        echo "<p> Baby, you're a [G] fire-[Am] work </p>"; 
+                        echo "<p> Come on, let your [Em] colors [C] burst </p>"; 
+                        echo "<p> Make 'em go, [G] oh oh [Am] oh </p>"; 
+                        echo "<p> You're gonna leave 'em all in [Em] awe, awe, [C] awe  </p>"; 
+                        echo "<br/>"; 
+                        echo "<p class='small'>[outro ]</p>"; 
+                        echo "<p> [G] Boom, boom, [Am] boom  </p>"; 
+                        echo "<p> Even brighter than the [Em] moon, moon, [C] moon </p>"; 
+                        echo "<p> [G] Boom, boom, [Am] boom</p>"; 
+                        echo "<p>  Even brighter than the [Em] moon, moon, [C] moon [G] </p>"; 
+                        echo "<br/>";
+                       
+                        ?>
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-dark">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Rose Ann Requintel</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
