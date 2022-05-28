03b1573a1b9c5df2ea28a2b7b48304b18788f9f8
RequintelRoseAnn <roseannrequintel7@gmail.com>
2022-05-28 17:39:16 +0800

    Song2 page Added

---
+++ song3.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <link href="css/styles.css" rel="stylesheet" />
+        <style>
+        body {
+                 background-color: darkblue;
+            }
+        </style>
+
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-2">
+                <a class="navbar-brand" href="#page-top">WebLyrics</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                       <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song_1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song_2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song_3</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song_4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song_5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Crazier</h1>
+                <p class="lead">Song by: Taylor Swift</p>
+                
+            </div>
+        
+
+        </header>
+
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-7 text-center text-white">
+                      
+                    
+                        <?php
+                                                //lyrics 
+                        echo "<h2 class='text-white'>'LYRICS'</h2>";   
+                        echo "<p class='lead'></p>"; 
+                        echo "<br/>"; 
+ 
+                        echo "<p class='small'>[Intro]</p>"; 
+                        echo "<p>[D] [A] [Bm] [G] [(G)]  </p>";
+                        echo "<br/>"; 
+ 
+                        echo "<p class='small'>[Verse 1]</p>"; 
+                        echo "<p>[D]I'd never gone with the [A]wind </p>";
+                        echo "<p> Just let it [Em]flow </p>";
+                        echo "<p> Let it [G]take me [(A)]where it wants to [D]go </p>";
+                        echo "<p> Till you open the [A]door there's so much [Em]more </p>";
+                        echo "<p> I've never [G]seen it before </p>"; 
+                        echo "<p> I was [A]trying to [Bm]fly but I [G]couldn't find wings </p>";
+                        echo "<p> But [A]you came [Bm]along and you [G]changed everything </p>";                         
+                        echo "<br/>"; 
+
+                        echo "<p class='small'>[Chorus ]</p>";                         
+                        echo "<p> [D]You lift my feet off the [A]ground </p>";
+                        echo "<p> You spin me [Bm]around </p>";
+                        echo "<p> You make me [G]crazier, crazier </p>";
+                        echo "<p>[D]Feels like I'm falling and [A]I </p>";
+                        echo "<p> I'm lost in your [Bm]eyes </p>";
+                        echo "<p> You make me [G]crazier, [(G)]crazier, crazier  </p>";                        
+                        echo "<br/>"; 
+ 
+                        echo "<p class='small'>[Verse 2]</p>"; 
+                        echo "<p> I [D]watched from a distance as [A]you  </p>";
+                        echo "<p> Made life your [Em]own </p>";
+                        echo "<p> Every [G]sky was [(A)]your own kind of [D]blue </p>";
+                        echo "<p> And I wanted to [A]know how that would [Em]feel  </p>";
+                        echo "<p> And you [G]made it so real </p>";
+                        echo "<p>[A]You showed me [Bm]something that [G]I couldn't see </p>";
+                        echo "<p> [A]You opened my [Bm]eyes and you [G]made me believe </p>";
+                        echo "<br/>"; 
+                        echo "<p class='small'>[Chorus ]</p>";                         
+                        echo "<p> [D]You lift my feet off the [A]ground </p>";
+                        echo "<p> You spin me [Bm]around </p>";
+                        echo "<p> You make me [G]crazier, crazier </p>";
+                        echo "<p>[D]Feels like I'm falling and [A]I </p>";
+                        echo "<p> I'm lost in your [Bm]eyes </p>";
+                        echo "<p> You make me [G]crazier, [(G)]crazier, crazier  </p>";                        
+                        echo "<br/>"; 
+ 
+                        echo "<p class='small'>[Interlude ]</p>";
+                        echo "<p> [D] [A] [G] [(G)] </p>";                        
+                        echo "<br/>"; 
+                        echo "<p class='small'>[Bridge ]</p>";
+                        echo "<p> [A]Baby you [Bm]showed me what [G]living is for </p>";
+                        echo "<p> I [A]don't want to [Bm]hide [G]anymore  </p>";                        
+                        echo "<br/>"; 
+                        echo "<p class='small'>[Chorus ]</p>";                         
+                        echo "<p> [D]You lift my feet off the [A]ground </p>";
+                        echo "<p> You spin me [Bm]around </p>";
+                        echo "<p> You make me [G]crazier, crazier </p>";
+                        echo "<p>[D]Feels like I'm falling and [A]I </p>";
+                        echo "<p> I'm lost in your [Bm]eyes </p>";
+                        echo "<p> You make me [G]crazier, [(G)]crazier, crazier  </p>";                           
+                        echo "<br/>";
+
+                    ?>
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-dark">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Rose Ann Requintel</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
